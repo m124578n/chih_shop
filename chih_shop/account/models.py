6b599538de2685222936527eb39c66529f7da73b
m124578n <m23568n@gmail.com>
2024-01-11 11:15:43 +0800

    add account models

--- chih_shop/account/models.py
+++ chih_shop/account/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
 # Create your models here.
+
+class Account(models.Model):
+    
+    class Sex(models.IntegerChoices):
+        FEMALE = 1, "Female"
+        MALE = 2, "Male"
+        OTHER = 3, "Other"
+    
+    class Status(models.IntegerChoices):
+        ACTIVATE = 1, "Activate"
+        FREEZE = 2, "Freeze"
+    
+    name = models.CharField(
+        verbose_name="name", 
+        max_lenght=50
+        )
+    
+    sex = models.PositiveSmallIntegerField(
+        verbose_name="sex",
+        choices=Sex,
+        default=Sex.OTHER
+        )
+    
+    phone = models.CharField(
+        verbose_name="phone", 
+        max_lenght=10
+        )
+    
+    email = models.EmailField(
+        verbose_name="email", 
+        max_lenght=254
+        )
+    
+    address = models.CharField(
+        verbose_name="address", 
+        max_lenght=100
+        )
+    
+    status = models.PositiveSmallIntegerField(
+        verbose_name="status",
+        choices=Status,
+        default=Status.ACTIVATE
+        )
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        de_table = 'account'
