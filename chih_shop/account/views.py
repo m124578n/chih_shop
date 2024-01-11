6d5a189634510a2be983dd128963b4811ff35ec7
m124578n <m23568n@gmail.com>
2024-01-11 12:37:04 +0800

    test django rest authentication

--- chih_shop/account/views.py
+++ chih_shop/account/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from django.shortcuts import render, get_object_or_404
+from .models import Account
 # Create your views here.
+
+
+@api_view(['GET'])
+def test_create(request):
+    user = request.user
+    user = Account.objects.filter(username=user).first()
+    return Response({'message': user.username})
+
