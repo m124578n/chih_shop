98fd1912d827eea9832000c23b2be0bb666ac83f
m124578n <m23568n@gmail.com>
2024-01-16 09:31:28 +0800

    update/ format

--- chih_shop/shop/views.py
+++ chih_shop/shop/views.py
@@ -3,14 +3,16 @@ from rest_framework import permissions
 from shop.models import Product
 from shop.serializers import ProductSerializer, CreateProductSerializer
 from shop.permissions import IsOwnerPermission
-from django.shortcuts import render
 
 # Create your views here.
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all().order_by("id")
     serializer_class = ProductSerializer
-    permission_classes = [IsOwnerPermission, permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [
+        IsOwnerPermission, 
+        permissions.IsAuthenticatedOrReadOnly
+        ]
 
     def get_serializer_class(self):
         if self.action == 'create':
